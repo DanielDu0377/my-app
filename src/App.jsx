18805861a4fd226c32955af8ce2aea3f5f60db1e
Daniel Du <d.du@wustl.edu>
2024-04-13 21:07:28 -0500

    Hello World

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
   return (
     <>
       Hello World
+      {/* Hello World */}
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
